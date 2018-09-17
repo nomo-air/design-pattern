d17514cc3d58020ce2ce6bc0f8ce0e11089d8844
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-17 16:50:22 +0800

    feat.开闭原则

---
+++ src/main/java/com/imooc/design/principle/openclose/ICourse.java
@@ -0,0 +1,7 @@
+package com.imooc.design.principle.openclose;
+
+public interface ICourse {
+    Integer getId();
+    String getName();
+    Double getPrice();
+}
---
+++ src/main/java/com/imooc/design/principle/openclose/Test.java
@@ -0,0 +1,10 @@
+package com.imooc.design.principle.openclose;
+
+public class Test {
+    public static void main(String[] args) {
+        ICourse iCourse = new JavaDiscountCourse(96, "Java从零到企业级电商开发", 348d);
+        JavaDiscountCourse javaCourse = (JavaDiscountCourse) iCourse;
+        System.out.println("课程ID：" + javaCourse.getId() + "，课程名字：" + javaCourse.getName() + "，课程价格：" + javaCourse.getPrice()
+                + "，课程原价：" + javaCourse.getOriginPrice());
+    }
+}
