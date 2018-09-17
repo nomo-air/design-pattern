c6013a7b900e250b55197fdc9cd014b218c71771
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-17 17:43:34 +0800

    feat.单一职责原则（类，修改后）

---
+++ src/main/java/com/imooc/design/principle/singleresponsibility/FlyBird.java
@@ -0,0 +1,7 @@
+package com.imooc.design.principle.singleresponsibility;
+
+public class FlyBird {
+    public void mainMoveMode(String birdName) {
+        System.out.println(birdName + "用翅膀飞");
+    }
+}
--- src/main/java/com/imooc/design/principle/singleresponsibility/Test.java
+++ src/main/java/com/imooc/design/principle/singleresponsibility/Test.java
@@ -2,8 +2,9 @@ package com.imooc.design.principle.singleresponsibility;
 
 public class Test {
     public static void main(String[] args) {
-        Bird bird = new Bird();
-        bird.mainMoveMode("大雁");
-        bird.mainMoveMode("鸵鸟");
+        FlyBird flyBird = new FlyBird();
+        flyBird.mainMoveMode("大雁");
+        WalkBird walkBird = new WalkBird();
+        walkBird.mainMoveMode("鸵鸟");
     }
 }
