50a742961919006053a8b142a4ec68203cbb9397
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-17 19:55:07 +0800

    feat.迪米特原则（修改后）

--- src/main/java/com/imooc/design/principle/demeter/Boss.java
+++ src/main/java/com/imooc/design/principle/demeter/Boss.java
@@ -1,14 +1,7 @@
 package com.imooc.design.principle.demeter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Boss {
     public void commandCheckNumber(TeamLeader teamLeader) {
-        List<Course> courseList = new ArrayList<>();
-        for (int i = 1; i <= 20; i++) {
-            courseList.add(new Course());
-        }
-        teamLeader.checkNumberOfCourses(courseList);
+        teamLeader.checkNumberOfCourses();
     }
 }
--- src/main/java/com/imooc/design/principle/demeter/TeamLeader.java
+++ src/main/java/com/imooc/design/principle/demeter/TeamLeader.java
@@ -1,9 +1,14 @@
 package com.imooc.design.principle.demeter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TeamLeader {
-    public void checkNumberOfCourses(List<Course> courseList) {
+    public void checkNumberOfCourses() {
+        List<Course> courseList = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            courseList.add(new Course());
+        }
         System.out.println("在线课程的数量是：" + courseList.size());
     }
 }
