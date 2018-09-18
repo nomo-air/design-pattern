74a1094a73b2d40efaaf61fbbeb1027ef90fb2cb
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-18 13:06:59 +0800

    feat.简单工厂（修改前）

---
+++ src/main/java/com/imooc/design/pattern/creational/simplefactory/Test.java
@@ -0,0 +1,8 @@
+package com.imooc.design.pattern.creational.simplefactory;
+
+public class Test {
+    public static void main(String[] args) {
+        Video video = new JavaVideo();
+        video.produce();
+    }
+}
