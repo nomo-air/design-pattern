4f0aa63fbc967b6972939f57ff675a676431a4f5
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-19 14:25:15 +0800

    feat.单例模式（双重检查，线程安全）

--- src/main/java/com/imooc/design/pattern/creational/singleton/MyThread.java
+++ src/main/java/com/imooc/design/pattern/creational/singleton/MyThread.java
@@ -4,8 +4,8 @@ package com.imooc.design.pattern.creational.singleton;
 public class MyThread implements Runnable {
     @Override
     public void run() {
-        LazySingleton lazySingleton = LazySingleton.getInstance();
-
-        System.out.println(Thread.currentThread().getName() + "，" + lazySingleton);
+        // LazySingleton lazySingleton = LazySingleton.getInstance();
+        LazyDoubleCheckSingleton instance = LazyDoubleCheckSingleton.getInstance();
+        System.out.println(Thread.currentThread().getName() + "，" + instance);
     }
 }
