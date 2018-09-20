4f39108d940e4ec718f43940c541c3789a924887
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-20 15:27:57 +0800

    feat.装饰者模式

---
+++ src/main/java/com/imooc/design/pattern/structural/decorator/v1/Battercake.java
@@ -0,0 +1,12 @@
+package com.imooc.design.pattern.structural.decorator.v1;
+
+
+public class Battercake {
+    protected String getDesc(){
+        return "煎饼";
+    }
+    protected int cost(){
+        return 8;
+    }
+
+}
---
+++ src/main/java/com/imooc/design/pattern/structural/decorator/v2/ABattercake.java
@@ -0,0 +1,10 @@
+package com.imooc.design.pattern.structural.decorator.v2;
+
+
+public abstract class ABattercake {
+
+    protected abstract String getDesc();
+
+    protected abstract int cost();
+
+}
