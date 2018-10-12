6cd694b70d96ebc57a52fff591314f1633ff401c
zhengwanlei <zhengwanlei@nibirutech.com>
2018-10-12 14:35:34 +0800

    feat.解释器模式（源码应用）

---
+++ src/main/java/com/imooc/design/behavioral/interpreter/SpringTest.java
@@ -0,0 +1,14 @@
+package com.imooc.design.behavioral.interpreter;
+
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class SpringTest {
+    public static void main(String[] args) {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("100 * 2 + 400 * 1 + 66");
+        int result = (int) expression.getValue();
+        System.out.println(result);
+    }
+}
